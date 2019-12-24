336d5761eb7359ea96006deb8917d5b418c04b8b
Alexander Römer <alex@roemer.design>
2019-12-24 10:11:00 +0100

    Initial commit

--- FaceID3DAnimation/ContentView.swift
+++ FaceID3DAnimation/ContentView.swift
@@ -9,8 +9,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showFirstStroke  = false
+    @State private var showSecondStroke = false
+    @State private var showCheckMark    = false
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack() {
+            RadialGradient(gradient: Gradient(colors: [Color.black, .black]), center: .center, startRadius: 5, endRadius: 500)
+            
+            Circle()
+                .strokeBorder(lineWidth: showFirstStroke ? 2 :  50, antialiased: false)
+                .frame(width: 100, height: 100)
+                .foregroundColor(showFirstStroke ? .green : .pink)
+                .rotation3DEffect(.degrees(showFirstStroke ? 0 : 360), axis: (x: 1, y: 1, z: 1))
+                .animation(Animation.easeInOut(duration: 2).delay(1))
+                .onAppear() {
+                    self.showFirstStroke.toggle()
+            }
+            
+            Circle()
+                .strokeBorder(lineWidth: showSecondStroke ? 2 :  50, antialiased: false)
+                .frame(width: 100, height: 100)
+                .foregroundColor(showFirstStroke ? .green : .pink)
+                .rotation3DEffect(.degrees(showSecondStroke ? 0 : 360), axis: (x: -1, y: 1, z: 1))
+                .animation(Animation.easeInOut(duration: 2).delay(1))
+                .onAppear() {
+                    self.showSecondStroke.toggle()
+            }
+            
+            Path { path in
+                path.move(to: CGPoint(x: 25, y: 45))
+                path.addLine(to: CGPoint(x: 25, y: 45))
+                path.addLine(to: CGPoint(x: 40, y: 60))
+                path.addLine(to: CGPoint(x: 70, y: 30))
+            }
+            .trim(from: 0, to: showCheckMark ? 1 : 0)
+            .stroke(style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round))
+            .foregroundColor(.white)
+            .offset(x: UIScreen.main.bounds.size.width / 2 - 47, y: UIScreen.main.bounds.size.height / 2 - 45)
+            .animation(Animation.easeInOut.delay(3))
+            .onAppear() {
+                self.showCheckMark.toggle()
+            }
+            
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
